263a7486da6b5d9555d38512e68c6e80496e8be0
Henry Martins <henrymartinsb@icloud.com>
2022-06-14 23:36:47 -0300

    api OK

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const server = express();
+
+server.get('/', (req,res) => {
+    return res.json({})
+});
+
+server.listen(3000, () => {
+    console.log('servidor funcionando')
+});
